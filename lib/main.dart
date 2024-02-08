a8f3a31a36ecb19b04a8397df54f53fa189cc20a
sjpark0765 <sjpark0765@gmail.com>
2024-02-08 23:45:49 +0900

    Fixed main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
-// import 'package:practice_flutter_apps/provider_1/provider_app_01.dart';
-import 'package:practice_flutter_apps/game_streaming/game_streaming.dart';
 
 void main() {
-  // runApp(const ProviderApp01());
-  runApp(const GameStreamingApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: true,
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Hello World!"),
+      ),
+      body: const Center(
+        child: Text(
+          "Hello World!",
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    ),
+  ));
 }
