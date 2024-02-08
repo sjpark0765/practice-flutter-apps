9a06b99544498c42293e77644f074d485dd33675
sjpark0765 <sjpark0765@gmail.com>
2024-02-08 23:39:49 +0900

    game streaming branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-// import 'package:practice_flutter_apps/provider_1/provider_app_01.dart';
+
 import 'package:practice_flutter_apps/game_streaming/game_streaming.dart';
 
 void main() {
-  // runApp(const ProviderApp01());
   runApp(const GameStreamingApp());
 }
