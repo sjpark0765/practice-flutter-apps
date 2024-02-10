2a18c6101992b3ac58c4944015cc7c3d89173ce3
sjpark0765 <sjpark0765@gmail.com>
2024-02-10 15:32:22 +0900

    firebase add

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
-import 'package:flutter/material.dart';
+import 'dart:js';
 
-void main() {
-  runApp(const EmptyApp());
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  runApp(const MessagerApp());
 }
 
-class EmptyApp extends StatelessWidget {
-  const EmptyApp({Key? key}) : super(key: key);
+class MessagerApp extends StatelessWidget {
+  const MessagerApp({super.key});
+
   @override
-  Widget build(BuildContext buildContext) {
-    return const MaterialApp(
-      home: Scaffold(
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true),
+      home: const Scaffold(
         body: Center(
-          child: Text("Empty APP1"),
+          child: Text("Messager App",
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.greenAccent,
+                  fontWeight: FontWeight.w900)),
         ),
       ),
     );
