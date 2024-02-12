3e946a745bb103ba0cfbf60ea881c9974c3d6d04
sjpark0765 <sjpark0765@gmail.com>
2024-02-12 15:42:45 +0900

    리소스 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,37 @@
-import 'dart:js';
-
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import "package:firebase_core/firebase_core.dart";
+import "package:flutter/material.dart";
+import "package:get/get.dart";
+import "package:practice_flutter_apps/firebase_options.dart";
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
-  runApp(const MessagerApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MessagerApp());
 }
 
 class MessagerApp extends StatelessWidget {
-  const MessagerApp({super.key});
+  const MessagerApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true),
-      home: const Scaffold(
-        body: Center(
-          child: Text("Messager App",
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.greenAccent,
-                  fontWeight: FontWeight.w900)),
+          useMaterial3: true,
         ),
-      ),
-    );
+        home: const Scaffold(
+          body: SafeArea(
+            child: Center(
+              child: Text(
+                "Messager App",
+                style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.deepOrange,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ));
   }
 }
